c212d289f3f3d059dc05ea37e8ac3c57fb326284
Michael Nikolaev <snownikmaikl@gmail.com>
2016-09-08 18:59:05 +0300

    Fixed color of widget

---
+++ MJournalWatch Extension/EvenDayInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  EvenDayInterfaceController.swift
+//  MJournal
+//
+//  Created by Michael Nikolaev on 31.08.16.
+//  Copyright © 2016 Ocode. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class EvenDayInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
